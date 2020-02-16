fe4592de1e9852bd7b75025836f1e2d1970b2ba2
Cynthia Lan <cynthia@crimsonvalley.dev>
2020-02-15 16:31:54 -0800

    cl - added keyboard handler & styling

--- ax/firepong.js
+++ ax/firepong.js
@@ -1,5 +1,30 @@
 <!-- FIREPONG GAME SCRIPT -->
 
+// Define keycode constants
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+
+// Initialize game state
+var playing = false;
+var pressed_up = false;
+var pressed_down = false
+
 function play() {
-	document.write("play game");
+	playing = true;
+}
+
+function keydown(event) {
+// Detect when the key has been pressed
+	if(playing) {
+		document.body.innerHTML = event.keyCode.toString();
+	} else {
+		play();
+	}
+}
+
+function keyup(event) {
+// Detect when the key has been released
+	if(playing) {
+		document.body.innerHTML = event.keyCode.toString();
+	}
 }
